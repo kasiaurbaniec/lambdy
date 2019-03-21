080ac7f45f4d699f05fe1a8f4015415f676e9809
student <kas@fake.pl>
2019-03-21 10:35:41 +0100

    4

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         method("Ala", "ma", "kota", (s1, s2, s3) -> s1.contains("a") && s2.contains("a") && s3.contains("a"));
         method("Ala", "ma", "kota", (s1, s2, s3) -> s1.length()==3 && s2.length()==3 && s3.length()==3);
-        method("Ala", "ma", "kota", (s1, s2, s3) -> s1.startsWith("t") && s2.startsWith("m") || s3.contains("a"));
+        method("Ala", "miała", "kota", (s1, s2, s3) -> s1.startsWith("t") && s2.startsWith("m") || s3.contains("a"));
         method("x", "y", "z", (s1, s2, s3) -> s1.equalsIgnoreCase("ala") && s2.length()==1 && s3.endsWith("ta"));
         method("x", "y", "x", (s1, s2, s3) -> s1.length()==1 || s2.length()==1 || s3.length()==1);
         method("Ala", "ma", "kota", (s1, s2, s3) -> s1.contains("b") && s2.contains(s1) && s3.contains("papap"));
