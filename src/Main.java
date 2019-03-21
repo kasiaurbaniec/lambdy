f0fcb545610ae6520aa93e325a25e185d3380e75
student <kas@fake.pl>
2019-03-21 09:31:37 +0100

    zmiany

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+
+
 public class Main {
     public static void main(String[] args) {
-
+method("Ala","ma","kota",(s1,s2,s3)->s1.contains("a")&&s2.contains("a")&&s3.contains("a"));
+method("Ala","ma","kota",(s1,s2,s3)->s1.startsWith("a")&&s2.startsWith("a")&&s3.startsWith("a"));
+method("Ala","ma","kota",(s1,s2,s3)->s1.contains("a")&&s2.contains("a")&&s3.contains("a"));
 
 
 
@@ -8,6 +12,9 @@ public class Main {
     interface MyOwnInteface{
         boolean go(String s1, String s2,String s3);
 
+    }
+    static void method(String s1, String s2, String s3, MyOwnInteface myOwnInteface){
+        System.out.println(myOwnInteface.go(s1,s2,s3));
     }
 
 }
